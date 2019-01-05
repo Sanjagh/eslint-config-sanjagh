cee8b75012309c078fcda99ac84daacbf82335ef
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-01-05 17:00:06 +0330

    Add variable style rules

--- rules/style.js
+++ rules/style.js
@@ -1,7 +1,19 @@
 module.exports = {
   rules: {
     semi: ['error', 'always'],
+
     'semi-spacing': ['error', { before: false, after: true }],
+
     'semi-style': ['error', 'last'],
+
+    'no-multi-assign': 'error',
+
+    'one-var': ['error', 'never'],
+
+    'no-plusplus': 'error',
+
+    'operator-linebreak': ['error', 'before', { overrides: { '=': 'none' } }],
+
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
   },
 };
