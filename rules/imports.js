3e9276156d5e255cdaa85a023144194c6a665ec1
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-01-05 18:11:02 +0330

    Add import rules

--- rules/imports.js
+++ rules/imports.js
@@ -1,3 +1,15 @@
 module.exports = {
+  plugins: [
+    'import'
+  ],
 
+  rules: {
+    'import/no-duplicates': 'error',
+
+    'import/no-mutable-exports': 'error',
+
+    'import/prefer-default-export': 'error',
+
+    'import/first': 'error',
+  },
 };
