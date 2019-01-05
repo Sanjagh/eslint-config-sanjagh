e16a3a7ddf395bb4a4835b17bde2a4fd579b7fc2
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-01-05 16:31:52 +0330

    Add basic es6 configs

---
+++ rules/es6.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      generators: false,
+    },
+  },
+};
