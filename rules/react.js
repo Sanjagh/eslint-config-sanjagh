0514a295bab83aecbbd7e9afce96cca2a84b5272
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-01-06 18:07:32 +0330

    Add react component type rules

--- rules/react.js
+++ rules/react.js
@@ -11,5 +11,10 @@ module.exports = {
 
   rules: {
     'react/no-multi-comp': ['error', { ignoreStateless: true }],
+
+    'react/prefer-es6-class': ['error', 'always'],
+
+    'react/prefer-stateless-function': ['error', { ignorePureComponents: true }],
+
   },
 };
