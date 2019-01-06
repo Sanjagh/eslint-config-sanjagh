c1a41e0b393cab061e2393c64f78a56ce5b98327
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-01-06 18:21:23 +0330

    Add react paran rule

--- rules/react.js
+++ rules/react.js
@@ -40,5 +40,15 @@ module.exports = {
     'react/no-array-index-key': 'error',
 
     'react/no-string-refs': 'error',
+
+    'react/jsx-wrap-multilines': ['error', {
+      declaration: 'parens-new-line',
+      assignment: 'parens-new-line',
+      return: 'parens-new-line',
+      arrow: 'parens-new-line',
+      condition: 'parens-new-line',
+      logical: 'parens-new-line',
+      prop: 'parens-new-line',
+    }],
   },
 };
